12e370673dd266df3f38fa3aae250338e9914af2
vilaca <jvilaca@gmail.com>
2022-02-16 21:59:26 +0000

    https://start.spring.io/

---
+++ src/main/java/eu/vilaca/whatsup/WhatsupApplication.java
@@ -0,0 +1,13 @@
+package eu.vilaca.whatsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/vilaca/whatsup/WhatsupApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.vilaca.whatsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
